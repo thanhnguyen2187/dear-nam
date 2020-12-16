701836364a82808ea6195c577174a99f41a4e17d
Thanh Nguyen <thanhnguyen2187@gmail.com>
2020-12-17 00:59:51 +0700

    Continue implementing

--- strings-length.py
+++ strings-length.py
@@ -15,18 +15,22 @@ def f(
             counter[len(item)] = 0
         counter[len(item)] += 1
 
-    # TODO: Change the implementation here to let it return an array of "lengths"
-    required_length, appearance_count = max(
-        counter.items(),
-        key=lambda item: item[1]
+    max_appearance_count = max(
+        counter.values()
+    )
+    required_lengths = set(
+        length
+        for length, count in counter.items()
+        if count == max_appearance_count
     )
     satisfied_strings = [
         item
         for item in L
-        if len(item) == required_length
+        if len(item) in required_lengths
     ]
 
     return satisfied_strings
+
 
 def unit_test():
     test_inputs = [
@@ -43,10 +47,11 @@ def unit_test():
     for test_input, test_output in zip(test_inputs, test_outputs):
         function_output = f(test_input)
 
-        print(test_input)
-        print(test_output)
-        print(function_output)
-        
+        print(f"="*25)
+        print(f"Test input: {test_input}")
+        print(f"Test output: {test_output}")
+        print(f"Function output: {function_output}")
+
         assert function_output == test_output
 
 unit_test()
